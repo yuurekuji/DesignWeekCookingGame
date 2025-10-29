14ceb7812a304198ec81102908cc9ce41fd2db46
yuurekuji <Kanoyxng@gmail.com>
2025-10-29 00:34:19 -0400

    Update IngredientCut.cs

--- Assets/Cooking Stuff/Scripts/IngredientCut.cs
+++ Assets/Cooking Stuff/Scripts/IngredientCut.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class IngredientCut : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject CutPiece;
+
     void Start()
     {
 
